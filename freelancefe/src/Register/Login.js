a33ab9508c7e095c34c08123a99d6e1de84a1286
Wei xiaoyu <61376976+Mango-Wei@users.noreply.github.com>
2022-08-06 08:05:57 +0800

    Add files via upload

---
+++ freelancefe/src/Register/Login.js
@@ -0,0 +1,77 @@
+import { Button, Form, Input,Radio } from 'antd';
+import React, { useState } from 'react';
+
+const Login = () => {
+  
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const onChange = (e)=>{
+    setEmployer(e.target.value)
+  }
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!'
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Login
+        </Button>
+      </Form.Item>
+
+    </Form>
+  );
+};
+
+export default Login;
