be5cfad4298033b343757cd7b1db42e1620ef191
jessicajfeng <110646748+jessicajfeng@users.noreply.github.com>
2022-08-10 14:19:55 -0700

    Create TestPage.js

---
+++ freelancefe/src/components/TestPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class TestPage extends React.Component {
+
+    render() {
+        const {jobs} = this.props;
+        return (
+            <h1>
+                Job ID
+            </h1>
+
+        );
+    }
+
+}
+
+export default TestPage;
